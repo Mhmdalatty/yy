fbb3d69a91c87d3150a3aa13884c770efab12630
Mhmdalatty <105403469+Mhmdalatty@users.noreply.github.com>
2022-06-25 07:40:40 +0300

    Update music_stream.py

--- program/music_stream.py
+++ program/music_stream.py
@@ -183,8 +183,6 @@ async def play_tg_file(c: Client, m: Message, replied: Message = None, link: str
 
 
 @Client.on_message(command(["شغل", "تشغيل", "/play", f"play@{BOT_USERNAME}"]) & other_filters)
-@check_blacklist()
-@require_admin(permissions=["can_manage_voice_chats", "can_delete_messages", "can_invite_users"], self=True)
 async def audio_stream(c: Client, m: Message):
     await m.delete()
     do = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMember?chat_id=@{UPDATES_CHANNEL}&user_id={m.from_user.id}").text
